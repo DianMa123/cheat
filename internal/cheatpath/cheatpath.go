ddb2c6028ea22e22db03eef3b677e2f0c5c2d71a
Chris Lane <chris@chris-allen-lane.com>
2019-12-28 10:26:31 -0500

    feat: created relocation notice
    Deleted all files, and posted a notice that the project has relocated.

--- internal/cheatpath/cheatpath.go
+++
@@ -1,9 +0,0 @@
-package cheatpath
-
-// Cheatpath encapsulates cheatsheet path information
-type Cheatpath struct {
-	Name     string   `yaml:name`
-	Path     string   `yaml:path`
-	ReadOnly bool     `yaml:readonly`
-	Tags     []string `yaml:tags`
-}
